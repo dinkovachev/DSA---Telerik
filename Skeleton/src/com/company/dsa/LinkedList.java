478ac7a6217fe1b36485bd1391d63e2960136eef
Dinko Kovachev <dinko.kovachev.a56@learn.telerikacademy.com>
2023-11-30 17:28:09 +0200

    Workshop2(Linked List) Init commit

---
+++ Skeleton/src/com/company/dsa/LinkedList.java
@@ -0,0 +1,83 @@
+package com.company.dsa;
+
+import java.util.Iterator;
+
+public class LinkedList<T> implements List<T> {
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public LinkedList(Iterable<T> iterable) {
+        iterable.forEach(this::addLast);
+    }
+
+    @Override
+    public void addFirst(T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addLast(T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T getFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T getLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T get(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T removeFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T removeLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    private class Node {
+        T value;
+        Node prev;
+        Node next;
+
+        Node(T value) {
+            this.value = value;
+        }
+    }
+}
